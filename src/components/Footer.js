a2dab26df03b09f83b6b815007928f7991cd89aa
JasmiAlankara <ajpjp13@gmail.com>
2025-04-06 20:57:35 +0530

    change footer to rediret to the social media link through icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,25 @@ import ReorderIcon from "@mui/icons-material/Reorder";
 import "../styles/Footer.css";
 
 function Footer() {
+  const openLink = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
+        <button className="icon-button" onClick={() => openLink("https://www.instagram.com/jasmi_alankarage/?hl=en")}>
+          <InstagramIcon />
+        </button>
+        <button className="icon-button" onClick={() => openLink("YOUR_TWITTER_URL")}> 
+          <TwitterIcon />
+        </button>
+        <button className="icon-button" onClick={() => openLink("https://www.facebook.com/profile.php?id=100090868064528&mibextid=ZbWKwL")}>
+          <FacebookIcon />
+        </button>
+        <button className="icon-button" onClick={() => openLink("https://www.linkedin.com/in/alankarage-jasmi-pavithra-b11099251/")}>
+          <LinkedInIcon />
+        </button>
       </div>
       <p> &copy; 2025 Jasmizpizza.com</p>
     </div>
