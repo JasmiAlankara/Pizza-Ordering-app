3e9e57b8eb8e57ca8203213db98f91f4f1227d36
JasmiAlankara <ajpjp13@gmail.com>
2025-04-06 20:48:14 +0530

    fixed issues

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,19 +11,20 @@ function About() {
       <div className="aboutBottom">
         <h1> ABOUT US</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
+        Welcome to Pizza Paradise, 
+        </p>
+        <p>
+        where we bring you the 
+        finest and freshest pizzas right to your doorstep! At 
+        Pizza Paradise, we are passionate about delivering delicious 
+        pizzas made with high-quality ingredients and a secret blend 
+        of spices that will make every bite a perfect experience. 
+        Whether you're craving a classic Margherita, a hearty meat feast, 
+        or a fresh vegetarian option, we have something to satisfy every taste. 
+        Our mission is to provide fast, friendly service and the best pizza in town, 
+        all from the comfort of your home. Order now and enjoy the best pizza in town, 
+        hot and fresh!
+        
         </p>
       </div>
     </div>
